fc5c06436619160c120a65b0dcab98916443495e
LucasNG521 <ntw521@gmail.com>
2018-06-18 20:11:41 +0800

    Notification and some interface/ code change

--- NavigationApp/src/screens/Notification.tsx
+++ NavigationApp/src/screens/Notification.tsx
@@ -4,7 +4,9 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity
+  TouchableOpacity,
+  FlatList,
+  Image
 } from 'react-native';
 import { Navigator } from 'react-native-navigation';
 
@@ -14,17 +16,66 @@ interface NotificationProps {
 
 export default class Notification extends React.Component<NotificationProps> {
 
+  // public renderNotificationItem(item) {
+  //   return (
+  //     <TouchableOpacity onPress={() => this.props.navigator.push({
+  //       screen: 'EventsTabScreen',
+  //     })}>
+  //       <View>
+  //         <Text>
+  //           {data.item.user} created new event {data.item.event}
+  //         </Text>
+  //       </View>
+  //     </TouchableOpacity>
+  //   )
+  // }
+
   public render() {
     return (
       <View>
         <TouchableOpacity onPress={() => this.props.navigator.switchToTab({
           tabIndex: 0
         })}>
-        <Text>
-          this is Notification
-        </Text>
+          <FlatList
+            
+            data={[{
+              id: 1,
+              user: 'Lucas',
+              event: 'Sex Party',
+              eventImg: 'https://dummyimage.com/600x400/000000/fff.png&text=Sex'
+            }, {
+              id: 2,
+              user: 'Brad',
+              event: 'Boat Party',
+              eventImg: 'https://dummyimage.com/600x400/000000/fff.png&text=Boat'
+            }]}
+            renderItem={(data) => {    
+              return (
+              <TouchableOpacity 
+                style={{borderBottomWidth: 0.5, }}
+                onPress={() => this.props.navigator.push({
+                screen: 'EventsTabScreen',
+              })}>
+                <View style={{ justifyContent: 'space-between',flexDirection: 'row', margin: 10 }}>
+                  <Image 
+                    style={{ borderRadius: 75, width: 70, height: 70,}}
+                    source={{uri: data.item.eventImg}}
+                    />
+                  <Text style={{marginHorizontal: 20, padding: 10}}>
+                    {data.item.user} created new event {data.item.event}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            )}}
+          />
         </TouchableOpacity>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  FlatListStyle: {
+    
+  }
+});
--- NavigationApp/src/screens/fakeData.tsx
+++ NavigationApp/src/screens/fakeData.tsx
@@ -2,15 +2,60 @@
 //   return fetch(`https://dummyimage.com/600x400/000000/fff.png&text=fake+img`)
 //       .then(response => Promise.all([response, response.json()]))
 // }
- const Events =  [{
+
+//Events for SearchPage
+const fakedata = [{
   id: 1,
-  name: 'event1',
-  img: `https://dummyimage.com/600x400/000000/fff.png&text=fake+img` 
-}, 
-{
+  name: 'Event 1',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E1'
+}, {
   id: 2,
-  name: 'event2',
-  img: `https://dummyimage.com/600x400/000000/fff.png&text=fake+img`
+  name: 'Event 2',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E2'
+}, {
+  id: 3,
+  name: 'Event 3',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E3'
+}, {
+  id: 4,
+  name: 'Event 4',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E4'
+}, {
+  id: 5,
+  name: 'Event 5',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E5'
+}, {
+  id: 6,
+  name: 'Event 6',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E6'
+}, {
+  id: 7,
+  name: 'Event 7',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E7'
+}, {
+  id: 8,
+  name: 'Event 8',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E8'
+}, {
+  id: 9,
+  name: 'Event 9',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E9'
+}, {
+  id: 10,
+  name: 'Event 10',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E10'
+}, {
+  id: 11,
+  name: 'Event 11',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E11'
+}, {
+  id: 12,
+  name: 'Event 12',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E12'
+}, {
+  id: 13,
+  name: 'Event 13',
+  img: 'https://dummyimage.com/600x400/000000/fff.png&text=E13'
 }]
 
 const event = {
@@ -37,4 +82,63 @@ const event = {
   }
 }
 
-export { Events, event };
+// Profile ToDoItem fakedata
+const TodoData = [{
+  id: 1,
+  item: 'Coke',
+  quality: 12,
+  eventname: 'Even 1',
+  isActive: true
+}, {
+  id: 2,
+  item: 'Coke',
+  quality: 12,
+  eventname: 'Even 1',
+  isActive: true
+}, {
+  id: 3,
+  item: 'Coke',
+  quality: 12,
+  eventname: 'Even 1',
+  isActive: false
+}, {
+  id: 4,
+  item: 'Coke',
+  quality: 102,
+  eventname: 'Even 2',
+  isActive: false
+}, {
+  id: 5,
+  item: 'Coke',
+  quality: 102,
+  eventname: 'Even 2',
+  isActive: false
+}, {
+  id: 6,
+  item: 'Coke',
+  quality: 12,
+  eventname: 'Even 1',
+  isActive: true
+}, {
+  id: 7,
+  item: 'Coke',
+  quality: 102,
+  eventname: 'Even 1',
+  isActive: false
+}, {
+  id: 8,
+  item: 'Coke',
+  quality: 102,
+  eventname: 'Even 2',
+  isActive: false
+}, {
+  id: 9,
+  item: 'Coke',
+  quality: 102,
+  eventname: 'Even 2',
+  isActive: false
+},]
+
+
+
+export { fakedata, event ,TodoData};
