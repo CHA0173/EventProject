90c7ca5f060ff11ced05471307ab6909816b0b8d
Pak Hei Chan <jacob35723763@gmail.com>
2018-06-29 10:54:37 +0800

    change get all events

--- backend/services/EventService.ts
+++ backend/services/EventService.ts
@@ -7,10 +7,53 @@ import { default as joinjs } from 'join-js';
 
 export default class EventService {
   private resultMaps: Array<Object>;
+  private resultMaps2: Array<Object>;
 
   constructor(private knex: Knex) {
     this.knex = knex;
     this.resultMaps = [
+      {
+        mapId: 'eventMap',
+        idProperty: 'id',
+        properties: ['name', 'description', 'datetime', 'photo', 'address', 'private_event', 'deposit'],
+        collections: [
+          // { name: 'events_users', mapId: 'events_usersMap', columnPrefix: 'events_users_' },
+          { name: 'todo', mapId: 'todoMap', columnPrefix: 'todo_' },
+          { name: 'attendees', mapId: 'attendeesMap', columnPrefix: 'attendees_' },
+          { name: 'discussion', mapId: 'discussionMap', columnPrefix: 'discussion_' }
+        ]
+      },
+      {
+        mapId: 'todoMap',
+        idProperty: 'id',
+        // properties: ['type'],
+        collections: [
+          { name: 'items', mapId: 'itemsMap', columnPrefix: 'items_' },
+        ]
+      },
+      {
+        mapId: 'itemsMap',
+        idProperty: 'id',
+        properties: ['id', 'name', 'quantity', 'completed', 'user_id', 'user_name']
+      },
+      {
+        mapId: 'attendeesMap',
+        idProperty: 'id',
+        properties: ['name', 'photo' ,'creator']
+      },
+      {
+        mapId: 'discussionMap',
+        idProperty: 'id',
+        properties: ['name', 'comment']
+      }
+    ]
+
+    this.resultMaps2 = [
+      {
+        mapId: 'parentMap',
+        properties: ['name'],
+        collections: [ {name: 'event', mapId: 'eventMap', columnPrefix: 'event_'} ]
+      },
       {
         mapId: 'eventMap',
         idProperty: 'id',
@@ -359,9 +402,59 @@ export default class EventService {
 
   //Completed
   getAll() {
+    // return this.knex("events")
+    //   .select("events.id as events_id", "events.name", "events.photo", "events.datetime")
+    //   .where("events.isactive", true);
+
     return this.knex("events")
-      .select("events.id as events_id", "events.name", "events.photo", "events.datetime")
-      .where("events.isactive", true);
+      .select(
+        "events.id           as parent_name",
+        "events.id          as event_id",
+        "events.name        as event_name",
+        "events.description as event_description",
+        "events.datetime    as event_datetime",
+        "events.photo       as event_photo",
+        "events.address     as event_address",
+        "events.private_event     as event_private",
+        "events.deposit     as event_deposit",
+
+        "events_users.creator as attendees_creator",
+
+        "todo.id            as todo_id",
+
+        "items.id           as items_id",
+        "items.name         as items_name",
+        "items.quantity     as items_quantity",
+        "items.users_id      as items_user_id",
+        "itemusers.name     as items_user_name",
+        "items.completed    as items_completed",
+
+        "users.id           as attendees_id",
+        "users.name         as attendees_name",
+        "users.photo        as attendees_photo",
+
+        "discussion.id      as discussion_id",
+        "discussionusers.name as discussion_name",
+        "discussion.comment  as discussion_comment"
+
+      )
+      .leftJoin("todo", "todo.events_id", "events.id")
+      .leftJoin("items", "items.todo_id", "todo.id")
+      .leftJoin("events_users", "events_users.events_id", "events.id")
+      .leftJoin("discussion", "discussion.events_id", "events.id")
+      .leftJoin("users as discussionusers", "discussion.users_id", "discussionusers.id")
+      .leftJoin("users as itemusers", "items.users_id", "itemusers.id")
+      .leftJoin("users", "events_users.users_id", "users.id")
+      // .where("events.id", eid)
+      // .whereIn('events.id',this.knex.select('events_id').from('events_users'))
+      // .andWhere("events_users.creator",true)
+      // .andWhere("events.isactive", true)
+      // .andWhere("items.isactive", true)
+      // .andWhere("discussion.isactive", true)
+      .then(result => {
+        // console.log(joinjs.mapOne(result, this.resultMaps, 'eventMap', 'event_'))
+        return (result && result.length > 0) ? joinjs.mapOne(result, this.resultMaps2, 'parentMap', 'parent_') : {};
+      })
   }
 
   addComment(user:any, body: any) {
